9d3b60510a60dd2dfee8417be1eca335e427efba
lurong <418083501@qq.com>
2015-09-29 14:26:15 +0800

    no message

---
+++ AlwaysChat/classes/ctrl/chatView/LRChatCtrl.h
@@ -0,0 +1,13 @@
+//
+//  LRChatCtrl.h
+//  AlwaysChat
+//
+//  Created by lurong on 15/9/29.
+//  Copyright © 2015年 lurong. All rights reserved.
+//
+
+#import "LCBaseCtrl.h"
+
+@interface LRChatCtrl : LCBaseCtrl
+
+@end
---
+++ AlwaysChat/classes/ctrl/chatView/LRChatCtrl.m
@@ -0,0 +1,105 @@
+//
+//  LRChatCtrl.m
+//  AlwaysChat
+//
+//  Created by lurong on 15/9/29.
+//  Copyright © 2015年 lurong. All rights reserved.
+//
+
+#import "LRChatCtrl.h"
+
+@interface LRChatCtrl ()<UITableViewDataSource,UITableViewDelegate>
+{
+    BOOL _isFirst;
+    UITableView *_tableView;
+    NSMutableArray *_dataArray;
+}
+
+@end
+
+@implementation LRChatCtrl
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:MAIN_REFRESH object:nil];
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView cellForRowAtIndexPath:indexPath].selected = NO;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"hehe"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"hehe"];
+    }
+    
+    
+    
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _dataArray.count;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 45;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return .5;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return .5;
+}
+
+-(void)buildLayout
+{
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.width, self.view.height) style:UITableViewStyleGrouped];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    [self.view addSubview:_tableView];
+    
+    _dataArray = LOGIN_USER.messageList;
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    if (_isFirst) {
+        
+        [self buildLayout];
+        
+        _isFirst = NO;
+    }
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _isFirst = YES;
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
